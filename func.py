b0938d14c1beb6e2f91652464c5dcb646a4d8762
igorbarn <igor.barn@yandex.ru>
2023-04-17 17:47:35 +0700

    Добавил код функции

---
+++ func.py
@@ -0,0 +1,11 @@
+def angle(a, b):
+  # Скалярное произведение двух векторов
+  scalar = np.dot(a,b)
+  # Нормирование первого и второго вектора
+  norma = np.linalg.norm(a)
+  normb = np.linalg.norm(b)
+  # Получение угла между двумя векторами в радианах
+  anglerad = np.arccos(norma/(norma*normb))
+  # Перевод в градусы
+  anglegrad = np.rad2deg(anglerad)
+  return (anglegrad, anglerad)
